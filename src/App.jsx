34f45ac54252a1cef92038b43c1d1d331a04ca16
au-lex <aulexc4d2@gmail.com>
2023-09-11 21:09:29 +0100

    installation of dependices

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Component/HeaderContainer/Header'
+import Hero from './Component/HeroContainer/Hero'
+
+const App = () => {
+  return (
+    <>
+   
+   <Header />
+   <Hero />
+    </>
+  )
+}
+
+export default App
